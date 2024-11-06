d5393a2869768826fbac0b20a68e7dd36e6d02aa
sun4205 <hanna4205@gmail.com>
2024-11-06 15:45:59 -0500

    weatherCard

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -2,30 +2,35 @@ import sunny from "../../assets/day/sunny.svg";
 import { weatherOptions, defaultWeatherOptions } from "../../utils/constants";
 import "./WeatherCard.css";
 
- console.log("Weather Data:", weatherData);
-
 function WeatherCard({ weatherData, defaultWeatherOptions }) {
   console.log("WeatherCard Props:", weatherData, defaultWeatherOptions);
-  const fliteredOptions = weatherOptions.filter((option) => {
+  const filteredOptions = weatherOptions.filter((option) => {
     return (
       option.day === weatherData?.isDay &&
       option.condition === weatherData?.condition
     );
   });
 
+  console.log("Weather Data:", weatherData);
+  console.log("Weather Options:", weatherOptions);
+
   let weatherOption;
-  if (fliteredOptions.length === 0) {
+  if (filteredOptions.length === 0) {
     weatherOption = defaultWeatherOptions[weatherData?.isDay ? "day" : "night"];
   } else {
     weatherOption = fliteredOptions[0];
   }
   console.log(weatherOptions[2].url);
+  console.log("Selected weatherOption:", weatherOption);
+  console.log("Image URL:", weatherOption?.url);
   return (
     <section className="weather-card">
       <p className="weather-card__temp"> {weatherData?.temp?.F} &deg; F</p>
       <img
-        className={weatherOption?.url}
-        src={weatherOption?.url}
+        className={`weather-card__image ${
+          weatherOption?.condition || "default-condition"
+        }`}
+        src={weatherOption?.url || "default-image-path"}
         alt={`Card showing ${weatherOption?.day ? "day" : "night"} time ${
           weatherOption?.condition
         } weather`}
