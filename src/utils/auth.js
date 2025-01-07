2e2fc56a8fa93c374052ef46bfaca070331c5447
sun4205 <hanna4205@gmail.com>
2025-01-07 17:07:17 -0500

    add auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,27 @@
+export const BASE_URL = "http://localhost:3001";
+
+export const register = (name, avatar, email, password ) => {
+  return fetch(`${BASE_URL}/auth/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, avatar, email, password }),
+  }).then((res) => {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  });
+};
+
+export const authorize = (email, password) => {
+  return fetch(`${BASE_URL}/auth/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then((res) => {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  });
+};
