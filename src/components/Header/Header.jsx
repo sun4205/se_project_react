4479cd6b611427f48447b916bc3973a31c09fee0
sun4205 <hanna4205@gmail.com>
2025-01-08 21:58:37 -0500

    update header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,23 @@
 import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.svg";
+import placeholder from "../../assets/placeholder.svg";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import { Link } from "react-router-dom";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+import { useContext } from "react";
 
-function Header({ openRegisterModal, weatherData,openLoginModal }) {
+function Header({ openRegisterModal, weatherData, openLoginModal }) {
+  const { currentUser } = useContext(CurrentUserContext);
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
     day: "numeric",
   });
+
+  const getPlaceholderAvatar = (name) => {
+    const firstLetter = name ? name[0].toUpperCase() : "?";
+    return <div className="header__avatar-placeholder">{firstLetter}</div>;
+  };
   return (
     <header className="header">
       <Link to="/">
@@ -31,15 +40,35 @@ function Header({ openRegisterModal, weatherData,openLoginModal }) {
       </div>
 
       <div className="header__user-container">
-      <button
-          onClick={openLoginModal}
-          type="button"
-          className="header__add-clothes-btn"
-        >
-          Log in
-        </button>
-
-        <img className="header__avatar" src={avatar} alt="Terrence Tegegine" />
+        {!currentUser ? (
+          <>
+            <button
+              onClick={openLoginModal}
+              type="button"
+              className="header__add-clothes-btn"
+            >
+              Log in
+            </button>
+            <img
+              className="header__avatar"
+              src={placeholder}
+              alt="Default avatar"
+            />
+          </>
+        ) : (
+          <>
+            <span className="header__user-name">{currentUser.name}</span>
+            {currentUser.avatar ? (
+              <img
+                className="header__avatar"
+                src={currentUser.avatar}
+                alt={currentUser.name}
+              />
+            ) : (
+              getPlaceholderAvatar(currentUser.name)
+            )}
+          </>
+        )}
       </div>
     </header>
   );
