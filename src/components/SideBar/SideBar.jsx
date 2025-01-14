9c94ec00499bc70a70a522dcbc47c8798ad4e079
sun4205 <hanna4205@gmail.com>
2025-01-14 11:23:13 -0500

    update sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -7,15 +7,19 @@ const SideBar = ({ handleLogout, changeCurrenUserData }) => {
   const { currentUser } = useContext(CurrentUserContext);
   return (
     <div className="sideBar">
-      <img className="sideBar__avatar" src={currentUser.avatarURL || avatar}  alt={currentUser.name} />
-      <p className="sideBar__userName">{currentUser.name}</p>
-      <button onClick={changeCurrenUserData} type="button">
+      <div className="sideBar__userInfo">
+      <img className="sideBar__avatar" src={currentUser.avatarURL || avatar}  alt={currentUser.name || "User Avatar"} />
+      <p className="sideBar__userName">{currentUser.name || "User Avatar"}</p>
+      </div>
+      <div className = "sideBar__editProfile">
+      <button className="sideBar__changeUserData" onClick={changeCurrenUserData} type="button">
         Change Profile Data
       </button>
-      <button onClick={handleLogout} type="button">
+      <button className = "sideBar__logout" onClick={handleLogout} type="button">
         Log Out
       </button>
-    </div>
+      </div>
+      </div>
   );
 };
 
