383b0f3dfb993a7f67586f2dc69d960ac3b967d5
sun4205 <hanna4205@gmail.com>
2025-01-19 14:49:06 -0500

    avatar useEffect

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
-import './Avatar.css';
+import React, { useState, useEffect } from "react";
+import "./Avatar.css";
 
 const Avatar = ({ avatar, name }) => {
   const [error, setError] = useState(false);
-  const userInitial = name ? name[0].toUpperCase() : "?"; 
+
+  const userInitial = name ? name[0].toUpperCase() : "?";
+
+  React.useEffect(() => {
+    if (avatar && avatar !== "" && error) {
+      setError(false);
+    }
+  }, [avatar]);
 
   return (
     <>
       {error ? (
-        <div className="avatar-placeholder">
-          {userInitial}
-        </div>
+        <div className="avatar-placeholder">{userInitial}</div>
       ) : (
         <img
           src={avatar}
